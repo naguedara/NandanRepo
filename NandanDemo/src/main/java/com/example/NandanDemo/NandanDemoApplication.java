57fe835fb7407478de48a1f6e8f60560edf751f6
naguedara <enranb@gmail.com>
2022-07-05 08:03:21 +0530

    added nandan demo springboot project

---
+++ NandanDemo/src/main/java/com/example/NandanDemo/NandanDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.NandanDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NandanDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NandanDemoApplication.class, args);
+	}
+
+}
